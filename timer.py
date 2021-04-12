c59052f20d4b3d357512a62d287c7b04c3910933
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-04-12 14:27:14 +0200

    V.0.02
    Training Timer added

--- timer.py
+++ timer.py
@@ -2,7 +2,7 @@
 
 import time
 import os
-
+import datetime
 
 class colors:
     BLACK = '\033[30;'
@@ -22,6 +22,8 @@ class bcolors:
     YELLOW = '43m'
     PURPLE = '45m'
     WHITE = '107m'
+    GREEN = '42m'
+    CYAN = '46m'
 
 def flush():
     os.system("clear")
@@ -31,6 +33,9 @@ def countdown(sec, text, color, bcolor, comming_up):
         print('\033[1m' + str(sec) + colors.RESET + '\t' + color + bcolor + text + colors.RESET)
         print('\t\033[100mCOMMING UP: ' + comming_up + colors.RESET)
         sec -= 1
+
+        print("\nTRAINING TIME:")
+        print(str(datetime.timedelta(seconds=int(time.time() - start_time))))
         time.sleep(1)
         flush()
     
@@ -40,6 +45,10 @@ def switch(comming_up):
 def rest(sec, comming_up):
    countdown(sec, "REST", colors.BLACK, bcolors.BLUE, comming_up)
 
+def cooldown(comming_up):
+   countdown(300, "COOLDOWN", colors.BLACK, bcolors.GREEN, "END")
+
+
 def monday_training():
     heavy_leg_workout = [["BULGARIAN SQUATS - LEFT LEG", 2],["BULGARIAN SQUATS - RIGHT LEG", 2],["SQUAT JUMPS", 2],["REVERSE LUNGES", 2],["ALTERNATING INCLINE HIP THRUSTS", 2]]
     light_chest_workout = [["PUSH UPS", 2], ["EXPLOSIVE PUSH UPS", 2], ["90 DEGREE HOLD", 2]]
@@ -48,7 +57,7 @@ def monday_training():
     warmup(heavy_leg_workout[0][0])
    
     #MAIN WORKOUT
-    cycles = 3
+    cycles = 1 
     while cycles > 1:
         cycle(heavy_leg_workout, light_chest_workout[0][0], colors.RED, bcolors.YELLOW)
         cycle(light_chest_workout, heavy_leg_workout[0][0], colors.BLACK, bcolors.MAGENTA)
@@ -62,10 +71,10 @@ def monday_training():
     # CORE WORKOUT
     cycles = 3
     while cycles > 1:
-        cycle(core_A_workout, core_A_workout[0][0], colors.GREEN, bcolors.BLACK)
+        cycle(core_A_workout, core_A_workout[0][0], colors.BLACK, bcolors.CYAN)
         rest(3, core_A_workout[0][0])
         cycles -= 1    
-    cycle(core_A_workout, "COOLDOWN", colors.GREEN, bcolors.BLACK)
+    cycle(core_A_workout, "COOLDOWN", colors.BLACK, bcolors.CYAN)
 
     cooldown()
 
@@ -77,11 +86,8 @@ def friday_training():
 
 def warmup(comming_up):
     warmup_exercises = [["ROPE JUMPING", 2],["DYNAMIC STRETCH", 2]]
-    cycle(warmup_exercises, comming_up, colors.BLUE, bcolors.BLACK)
+    cycle(warmup_exercises, comming_up, colors.BLACK, bcolors.MAGENTA)
     switch(comming_up)
-
-def cooldown():
-   countdown(300, "COOLDOWN", colors.BLACK, bcolors.BLUE, comming_up)
 
 def cycle(exercises_table, first_exercise_in_next_cycle, color, bcolor): 
     for exercise, index in zip(exercises_table, range(len(exercises_table))):
@@ -91,5 +97,6 @@ def cycle(exercises_table, first_exercise_in_next_cycle, color, bcolor):
         else:
             countdown(exercise[1], exercise[0] , color, bcolor, first_exercise_in_next_cycle)
 
+start_time = time.time()
 flush()
 monday_training()
