4dace663a6ab81d51c2993a40d0b1568fcca166c
dd <54325660+sud0k1ller@users.noreply.github.com>
2021-04-21 19:14:43 +0200

    New idea implemented

--- timer.py
+++ timer.py
@@ -3,7 +3,6 @@
 import time
 import os
 import datetime
-import keyboard
 
 class colors:
     BLACK = '\033[30;'
@@ -29,111 +28,89 @@ class bcolors:
 def flush():
     os.system("clear")
 
-def handle_keys_press():
-    if keyboard.is_pressed('space'):
-        flush()
-        print("Press [space] to continue training...")
-        keyboard.wait('space')
-        flush()	
+def switch(comming_up):
+    exercise(1, "SWITCH", colors.BLACK, bcolors.WHITE, comming_up)
 
-def countdown(sec, text, color, bcolor, comming_up):
-    while sec:
-        handle_keys_press()
-        print('\033[1m' + str(sec) + colors.RESET + '\t' + color + bcolor + text + colors.RESET)
+def rest(sec, comming_up):
+    exercise(sec, "REST", colors.BLACK, bcolors.BLUE, comming_up)
+
+def training(cycles_list):
+    exercise(5, "COUNTDOWN", colors.BLACK, bcolors.WHITE, "WARMUP")
+    
+    for cycle_number in range(0, len(cycles_list) - 1):
+        cycle_repetitions = cycles_list[cycle_number][2]
+        for repetition in range(cycle_repetitions - 1):
+            training_cycle(cycles_list[cycle_number][0], colors.RED, bcolors.YELLOW)            
+            rest(cycles_list[cycle_number][1], cycles_list[cycle_number][0][0][0])
+        training_cycle(cycles_list[cycle_number][0], colors.RED, bcolors.YELLOW)            
+        rest(cycles_list[cycle_number][1], cycles_list[cycle_number + 1][0][0][0]) 
+    training_cycle(cycles_list[-1][0], colors.RED, colors.YELLOW)
+
+def training_cycle(exercise_table, color, bcolor):
+    for exercise_number in range(len(exercise_table) - 1):
+        exercise(exercise_table[exercise_number][1], exercise_table[exercise_number][0], colors.RED, bcolors.YELLOW, exercise_table[exercise_number + 1][0])
+        switch(exercise_table[exercise_number + 1][0])
+    exercise(exercise_table[-1][1], exercise_table[-1][0], colors.RED, bcolors.YELLOW, "REST")
+
+
+def exercise(exercise_time, exercise_name, color, bcolor, comming_up):
+    while exercise_time:
+        #handle_pause()
+        print('\033[1m' + str(exercise_time) + colors.RESET + '\t' + color + bcolor + exercise_name + colors.RESET)
         print('\t\033[100mCOMMING UP: ' + comming_up + colors.RESET)
-        sec -= 1
+        exercise_time -= 1
         print("\nTRAINING TIME:")
         print(str(datetime.timedelta(seconds=int(time.time() - start_time))))
+        print("\n\nTIME LEFT:")
+        print("00:00:00")
+        print("\n\n[SPACE] Pause/Resume")
         time.sleep(1)
         flush()    
 
-def switch(comming_up):
-   countdown(5, "SWITCH", colors.BLACK, bcolors.WHITE, comming_up)
+def count_training_time(cycles_list):
+    training_time = 0
 
-def rest(sec, comming_up):
-   countdown(sec, "REST", colors.BLACK, bcolors.BLUE, comming_up)
-
-def cooldown():
-   countdown(300, "COOLDOWN", colors.BLACK, bcolors.GREEN, "END")
-
-
-def monday_training():
-    heavy_leg_workout = [["BULGARIAN SQUATS - LEFT LEG", 60],["BULGARIAN SQUATS - RIGHT LEG", 60],["SQUAT JUMPS", 60],["REVERSE LUNGES", 60],["ALTERNATING INCLINE HIP THRUSTS", 60]]
-    light_chest_workout = [["PUSH UPS", 60], ["EXPLOSIVE PUSH UPS", 40], ["90 DEGREE HOLD", 20]]
-    core_A_workout = [["HIP LIFTS", 60], ["SIDE PLANK - LEFT SIDE", 30] , ["SIDE PLANK - RIGHT SIDE", 30], ["RUSSIAN TWISTS", 60]]
-    
-    countdown(5, "COUNTDOWN", colors.BLACK, bcolors.WHITE, "WARMUP")
-    
-    #WARMUP
-    warmup(heavy_leg_workout[0][0])
-    
-    #MAIN WORKOUT
-    cycles = 3 
-    while cycles > 1:
-        cycle(heavy_leg_workout, light_chest_workout[0][0], colors.RED, bcolors.YELLOW)
-        cycle(light_chest_workout, heavy_leg_workout[0][0], colors.BLACK, bcolors.MAGENTA)
-        rest(60, heavy_leg_workout[0][0])
-        cycles -= 1    
-    
-    cycle(heavy_leg_workout, light_chest_workout[0][0], colors.RED, bcolors.YELLOW)
-    cycle(light_chest_workout, core_A_workout[0][0], colors.BLACK, bcolors.MAGENTA)
-
-    
-    # CORE WORKOUT
-    cycles = 3
-    while cycles > 1:
-        cycle(core_A_workout, core_A_workout[0][0], colors.BLACK, bcolors.CYAN)
-        rest(30, core_A_workout[0][0])
-        cycles -= 1    
-    cycle(core_A_workout, "COOLDOWN", colors.BLACK, bcolors.CYAN)
-
-    cooldown()
-
-def wednesday_training():
-    back_and_arms_workout = [["SUPERMANS", 60], ["TOWEL PULL UPS", 60], ["TYI", 60], ["ONE HANDED DOORPOST ROWS - RIGHT SIDE", 30], ["ONE HANDED DOORPOST ROWS - LEFT SIDE", 30],["DIAMOND PUSH UPS", 60], ["REVERSE PUSH UPS", 60],["PIKE PUSH UPS", 60]]
-    core_B_workout = [["KNEE TO ELBOW IN PLANK", 60], ["SCISSORS", 60], ["STRAIGHT LEGS LIFTS", 60]]
-    
-    countdown(5, "COUNTDOWN", colors.BLACK, bcolors.WHITE, "WARMUP")
-    
-    #WARMUP
-    warmup(back_and_arms_workout[0][0])
-    
-    #MAIN WORKOUT
-    cycles = 3 
-    while cycles > 1:
-        cycle(back_and_arms_workout, back_and_arms_workout[0][0], colors.RED, bcolors.YELLOW)
-        rest(60, back_and_arms_workout[0][0])
-        cycles -= 1    
-    
-    cycle(back_and_arms_workout, core_B_workout[0][0], colors.BLACK, bcolors.MAGENTA)
-
-    # CORE WORKOUT
-    cycles = 3
-    while cycles > 1:
-        cycle(core_B_workout, core_B_workout[0][0], colors.BLACK, bcolors.CYAN)
-        rest(30, core_B_workout[0][0])
-        cycles -= 1    
-    cycle(core_B_workout, "COOLDOWN", colors.BLACK, bcolors.CYAN)
-
-    cooldown()
-
-def friday_training():
+    for rest_time in
     pass
 
-def warmup(comming_up):
-    warmup_exercises = [["ROPE JUMPING", 120],["DYNAMIC STRETCH", 180]]
-    cycle(warmup_exercises, comming_up, colors.BLACK, bcolors.MAGENTA)
-    switch(comming_up)
+##MAIN
+warmup_exercises =      [["ROPE JUMPING", 2],
+                        ["DYNAMIC STRETCH", 2]]
 
-def cycle(exercises_table, first_exercise_in_next_cycle, color, bcolor): 
-    for exercise, index in zip(exercises_table, range(len(exercises_table))):
-        if index+1 < len(exercises_table):
-            countdown(exercise[1],  exercise[0] , color, bcolor, exercises_table[index+1][0])
-            switch(exercises_table[index+1][0])
-        else:
-            countdown(exercise[1], exercise[0] , color, bcolor, first_exercise_in_next_cycle)
-            #switch(first_exercise_in_next_cycle)
-            
+heavy_leg_workout = 	[["BULGARIAN SQUATS - LEFT LEG", 60],
+				        ["BULGARIAN SQUATS - RIGHT LEG", 60],
+				        ["SQUAT JUMPS", 60],
+				        ["REVERSE LUNGES", 60],
+				        ["ALTERNATING INCLINE HIP THRUSTS", 60]]
+    
+light_chest_workout = 	[["PUSH UPS", 60], 
+				        ["EXPLOSIVE PUSH UPS", 40], 
+				        ["90 DEGREE HOLD", 20]]
+    
+   
+back_and_arms_workout = [["SUPERMANS", 2], 
+			            ["TOWEL PULL UPS", 2], 
+				        ["TYI", 2], 
+				        ["ONE HANDED DOORPOST ROWS - RIGHT SIDE", 2], 
+				        ["ONE HANDED DOORPOST ROWS - LEFT SIDE", 2],
+				        ["DIAMOND PUSH UPS", 2], 
+				        ["REVERSE PUSH UPS", 2],
+				        ["PIKE PUSH UPS", 2]]
+
+core_A_workout = 		[["HIP LIFTS", 60], 
+				        ["SIDE PLANK - LEFT SIDE", 30] , 
+				        ["SIDE PLANK - RIGHT SIDE", 30], 
+				        ["RUSSIAN TWISTS", 60]]
+    
+core_B_workout = 		[["KNEE TO ELBOW IN PLANK", 2], 
+				        ["SCISSORS", 2], 
+				        ["STRAIGHT LEGS LIFTS", 2]]
+
+cooldown =              [["COOLDOWN", 300]]
+
 start_time = time.time()
 flush()
-monday_training()
+
+#cycle_name, rest_time, cycle_repetition_count
+training([[warmup_exercises, 15, 1],[back_and_arms_workout, 3, 3], [core_B_workout, 3, 3], [cooldown, 0, 1])
+
